f9fafa9488a0e690ed9bb0b04fc1bc10b9ffdae8
MedBLM588 <mohamedboulaalam01@gmail.com>
2025-01-13 18:19:34 +0100

    Création des services pour MongoDB (mongoService.js) et Redis (redisService.js)

--- src/services/mongoService.js
+++ src/services/mongoService.js
@@ -5,10 +5,15 @@
 
 const { ObjectId } = require('mongodb');
 
-// Fonctions utilitaires pour MongoDB
+/**
+ * Recherche un document par son ID dans la collection MongoDB
+ * @param {Collection} collection - Collection MongoDB
+ * @param {string} id - ID au format string
+ * @returns {Object} - Document trouvé ou null
+ */
 async function findOneById(collection, id) {
   try {
-    const _id = new ObjectId(id); 
+    const _id = new ObjectId(id);
     return await collection.findOne({ _id });
   } catch (error) {
     console.error('Erreur lors de la recherche par ID :', error);
@@ -16,7 +21,23 @@ async function findOneById(collection, id) {
   }
 }
 
+/**
+ * Insère un document dans la collection MongoDB
+ * @param {Collection} collection
+ * @param {Object} doc - Le document à insérer
+ * @returns {Object} - Résultat de l'insertion
+ */
+async function insertOne(collection, doc) {
+  try {
+    return await collection.insertOne(doc);
+  } catch (error) {
+    console.error('Erreur lors de l\'insertion du document :', error);
+    throw error;
+  }
+}
+
 // Export des services
 module.exports = {
   findOneById,
+  insertOne,
 };
